ff1aeae1d10d1c3532f99e6ac6f22cf194ff4bba
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-05-13 14:06:11 -0400

    added models and migrations

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 __author__ = 'workhorse'
+import os
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "KrisKrosMakeYouWannaJumpJump"
@@ -10,3 +11,7 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir,"app.db")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
