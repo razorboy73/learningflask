d1278968457197b2d033caab87fbfc95772c9591
razorboy73 <josh.kerbel@femme-o-nomics.com>
2015-05-15 13:53:18 -0400

    fixed email issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'workhorse'
 import os
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from config import basedir, ADMINS
 from flask import Flask
 
 from flask.ext.login import LoginManager
@@ -19,15 +19,7 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
-if not app.debug:
-    import logging
-    from logging.handlers import SMTPHandler
-    credentials = None
-    if MAIL_USERNAME or MAIL_PASSWORD:
-        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+
 
 
 
